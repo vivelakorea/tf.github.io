c1ae64a872d7422558a6f06cb019e91c73404b01
vivelakorea <gyujang95@snu.ac.kr>
2022-01-24 13:33:48 +0900

    test3

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 async function start() {
   console.log('start')
-  const model = await tf.loadLayersModel('model/model.json')
+  const model = await tf.loadLayersModel('./model/model.json')
   console.log('done')
   console.log(model)
 }
